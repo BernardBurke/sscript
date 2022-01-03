c4db6d250e7247efe7686f556784580b25285acc
Ben Burke <bernard.francis.burke@gmail.com>
2022-01-04 08:13:26 +1100

    enhanced one of each to split to three output files

--- one_of_each.sh
+++ one_of_each.sh
@@ -32,6 +32,10 @@ fi
 
 OUTPUT_FILE="$OUTPUT_DIR/one_of_each.edl"
 
+OUTPUT_FILE1="$OUTPUT_DIR/one_of_each_1.edl"
+OUTPUT_FILE2="$OUTPUT_DIR/one_of_each_2.edl"
+OUTPUT_FILE3="$OUTPUT_DIR/one_of_each_3.edl"
+
 echo "Input $INPUT_DIR output $OUTPUT_FILE"
 
 
@@ -53,14 +57,34 @@ cp -v $OUTPUT_FILE $OUTPUT_DIR/one_of_each_$$.edl
 
 echo "# mpv EDL v0" > $OUTPUT_FILE
 
+echo "# mpv EDL v0" > $OUTPUT_FILE1
+echo "# mpv EDL v0" > $OUTPUT_FILE2
+echo "# mpv EDL v0" > $OUTPUT_FILE3
+
+
 for file in $INPUT_DIR/*.edl; do
 
     shuf -n $GET_ME  "$file" | grep -v "#"  >> $TMPFILE2
 
 done
 
+NO_OF_RECORDS=$(cat $TMPFILE2 | wc -l )
+
+echo "There were $NO_OF_RECORDS in the $OUTPUT_FILE"
+
+THIRDS=$(( $NO_OF_RECORDS / 3 ))
+
+echo "Split files are $THIRDS long"
+
+
 shuf $TMPFILE2 >> $OUTPUT_FILE
 
 cat $OUTPUT_FILE
 
 echo $OUTPUT_FILE
+
+shuf -n $THIRDS $TMPFILE2 >> $OUTPUT_FILE1
+shuf -n $THIRDS $TMPFILE2 >> $OUTPUT_FILE2
+shuf -n $THIRDS $TMPFILE2 >> $OUTPUT_FILE3
+
+echo "$THIRDS length written for 3 output files"
