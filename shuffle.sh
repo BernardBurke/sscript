8eb0155725799f31d2c8f4a386ec50704f9cde72
Ben Burke <bernard.francis.burke@gmail.com>
2022-01-23 11:26:32 +1100

    shuffle the keydir

---
+++ shuffle.sh
@@ -0,0 +1,8 @@
+# /bin/bash
+OUTFILE=$EDLWINSCRATCH/shuffle_$$.edl
+find $KEYCUTWIN -type f -iname '*.edl'| sort -R | shuf -n 9 | while read file; do
+	cat "$file" >> $OUTFILE
+    # Something involving $file, or you can leave
+    # off the while to just get the filenames
+done
+$SRC/shake_an_edl.sh $OUTFILE
