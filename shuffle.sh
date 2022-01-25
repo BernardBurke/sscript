86791f954c08caccbe921c04950a733bdb00e01b
Ben Burke <bernard.francis.burke@gmail.com>
2022-01-26 08:16:12 +1100

    altered shuffle to parametered source

--- shuffle.sh
+++ shuffle.sh
@@ -1,6 +1,25 @@
 # /bin/bash
-OUTFILE=$EDLWINSCRATCH/shuffle_$$.edl
-find $KEYCUTWIN -type f -iname '*.edl'| sort -Ru | shuf -n 9 | while read file; do
+source $SRC/common_inc.sh
+
+if [[ "$1" = "" ]]; then
+    INPUT_DIR=$KEYDIR
+    OUTPUT_DIR=$SCRATCHDIR
+else
+    INPUT_DIR=$1
+    OUTPUT_DIR=$SCRATCHDIR
+
+fi
+
+
+if [[ "$2" = "" ]]; then
+   SHUF_COUNT=10 
+else
+   SHUF_COUNT=10
+fi
+
+OUTFILE=$SCRATCHDIR/shuffle_$$.edl
+
+find $KEYCUTWIN -type f -iname '*.edl'| sort -Ru | shuf -n $SHUF_COUNT | while read file; do
 	cat "$file" >> $OUTFILE
     # Something involving $file, or you can leave
     # off the while to just get the filenames
