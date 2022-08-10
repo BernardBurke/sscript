0304b160f2d4bf6fd25a5e317083d8cb60ff558b
Ben Burke <bernard.francis.burke@gmail.com>
2022-08-10 20:51:47 +1000

    new file

---
+++ vosk_me.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# use vosk-transcribe to make subtitles for a single file
+source $SRC/common_inc.sh
+
+if [[ ! -f "$1" ]]; then
+    echo "Please provice an media file that exists"
+    exit 1
+fi
+
+INPUT_DIR=basename
+
+if [[ "$2" = "" ]]; then
+        OUTPUT_DIR=$(dirname "$1")
+else
+    if [[ -d $2 ]]; then
+        OUTPUT_DIR=$2
+    else
+        echo "$2 is not a valid directory "
+        exit 1
+    fi
+fi
+
+SRT_FILENAME=$OUTPUT_DIR/$(basename "$1" .mp4).srt
+
+echo $SRT_FILENAME
+
+if [[ "$3" = "" ]]; then
+        if [[ -f $SRT_FILENAME ]]; then
+            echo "$SRT_FILENAME exists and parameter 3 is blank - exiting"
+            exit 1
+        fi
+fi
+
+vosk-transcriber -i "$1" \
+        -t srt \
+        -o "$SRT_FILENAME" \
+        --model-name=vosk-model-en-us-0.22 
+
